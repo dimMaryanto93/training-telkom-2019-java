93e24ca9fad84b9994881f76cfcbdbcb9b757afa
dimasm93 <software.dimas_m@icloud.com>
2019-08-26 10:53:39 +0700

    tipe data byte

--- src/main/java/com/training/telkom/BelajarJava.java
+++ src/main/java/com/training/telkom/BelajarJava.java
@@ -3,6 +3,12 @@ package com.training.telkom;
 public class BelajarJava {
 
     public static void main(String[] args) {
-        System.out.println("Halo dunia");
+        byte bil1 = -128;
+        Integer bil2 = 0;
+        System.out.println(
+                "nilai dari bil1 = " +
+                        bil1 +
+                        ", nilai dari bil2 = " +
+                        bil2);
     }
 }
