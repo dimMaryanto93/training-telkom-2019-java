d7f82d23f3ff47ef2b8beb611a2db6f9326e5941
dimasm93 <software.dimas_m@icloud.com>
2019-08-27 10:33:04 +0700

    setter & getter

--- src/main/java/com/training/telkom/Mobil.java
+++ src/main/java/com/training/telkom/Mobil.java
@@ -2,10 +2,27 @@ package com.training.telkom;
 
 public class Mobil extends Kendaraan {
 
+    private String stering;
+    private Integer jumlahKorsi;
+
     public Integer jumlahRoda() {
         roda = 4;
         return roda;
     }
 
+    public String getStering() {
+        return stering;
+    }
 
+    public void setStering(String stering) {
+        this.stering = stering;
+    }
+
+    public Integer getJumlahKorsi() {
+        return jumlahKorsi;
+    }
+
+    public void setJumlahKorsi(Integer jumlahKorsi) {
+        this.jumlahKorsi = jumlahKorsi;
+    }
 }
