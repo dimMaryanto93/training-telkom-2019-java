717c314417e322d2f8148be37ca7b8aaadc60989
dimasm93 <software.dimas_m@icloud.com>
2019-08-27 09:50:29 +0700

    cleanup

--- src/main/java/com/training/telkom/BelajarJava.java
+++ src/main/java/com/training/telkom/BelajarJava.java
@@ -3,19 +3,7 @@ package com.training.telkom;
 public class BelajarJava {
 
     public static void main(String[] args) {
-        String stringValue = new String("asldfkjdsfksdf");
-        BelajarJava variable = new BelajarJava();
-        variable.ping();
 
-        Integer hitungLuasPersegi = variable.luasPersegi(10, 20);
-        System.out.println("luas persegi panjang " + hitungLuasPersegi);
     }
 
-    public void ping() {
-        System.out.println("connected to database!");
-    }
-
-    public Integer luasPersegi(Integer panjang, Integer lebar) {
-        return panjang * lebar;
-    }
 }
